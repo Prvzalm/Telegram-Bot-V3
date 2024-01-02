35efd1f1a28942fcf8ca6fd40faa9705a97dc154
Parvez Alam <prvzalm69@gmail.com>
2024-01-02 12:07:39 +0530

    removed TOKEN from index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { message } =  require('telegraf/filters')
 
 const app = express();
 
-const botToken = '6370448394:AAGaA-FFH92-lylUkKBPF321PkuCppcVBug';
+const botToken = '';
 
 const bot = new Telegraf(botToken);
 
